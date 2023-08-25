f575ff830c933a950d6606f2cec275c321e16b9d
rjavm9 <142381873+rjavm9@users.noreply.github.com>
2023-08-25 21:25:52 +0900

    [ADD]BoardCourse, BoardLike 엔티티 클래스 작성 완료

--- src/main/java/com/walk/aroundyou/domain/BoardCourse.java
+++ src/main/java/com/walk/aroundyou/domain/BoardCourse.java
@@ -1,5 +1,38 @@
 package com.walk.aroundyou.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="board_course")
 public class BoardCourse {
 
+	@Id
+	@Column(name="bcourse_id", columnDefinition="bigint", nullable=false)
+	private long bcourseId;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "board_id", nullable = false)
+	private Board boardId;
+	
+	@ManyToOne
+	@JoinColumn(name = "esntl_id", nullable = false)
+	private Course esntlId;
+	
 }
+
