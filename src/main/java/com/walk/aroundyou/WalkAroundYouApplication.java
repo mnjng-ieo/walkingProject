b17a761524b5977c407cff83284620ae3629bc57
mnjng-ieo <mnjng.ieo@gmail.com>
2023-08-25 15:50:50 +0900

    first commit

---
+++ src/main/java/com/walk/aroundyou/WalkAroundYouApplication.java
@@ -0,0 +1,13 @@
+package com.walk.aroundyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkAroundYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkAroundYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walk/aroundyou/WalkAroundYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walk.aroundyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkAroundYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
