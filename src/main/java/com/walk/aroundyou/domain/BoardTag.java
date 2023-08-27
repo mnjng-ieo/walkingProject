29917d57544ed1647e31ad067996df0466c6eeb0
kay <kay@DESKTOP-OEPJ916>
2023-08-27 12:08:12 +0900

    [ADD] BoardTag, Hashtag 엔티티 클래스 작성 완료

--- src/main/java/com/walk/aroundyou/domain/BoardTag.java
+++ src/main/java/com/walk/aroundyou/domain/BoardTag.java
@@ -14,6 +14,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+
 @Entity
 @Data
 @AllArgsConstructor
--- src/main/java/com/walk/aroundyou/domain/Hashtag.java
+++ src/main/java/com/walk/aroundyou/domain/Hashtag.java
@@ -16,6 +16,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+
 @Entity
 @Data
 @AllArgsConstructor
