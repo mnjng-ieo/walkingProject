1240f05da2c6ee47e44f80a3f6e0ab20678d11a3
mnjng-ieo <mnjng.ieo@gmail.com>
2023-08-28 12:07:20 +0900

    Entity 클래스 수정 - 어노테이션 수정, Enum클래스 빨간줄 대비 코드 작성

--- src/main/java/com/walk/aroundyou/domainenum/StateId.java
+++ src/main/java/com/walk/aroundyou/domainenum/StateId.java
@@ -2,8 +2,11 @@ package com.walk.aroundyou.domainenum;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
+import net.bytebuddy.asm.Advice.Return;
 
 @Getter
+@ToString
 @AllArgsConstructor
 public enum StateId {
 	NORMAL("이상없음"),
@@ -15,4 +18,14 @@ public enum StateId {
 	
 	private final String kor;
 	
+	//// 아래 코드는 ENUM 클래스에 빨간줄이 나오면 작성해야할 코드입니다
+	//// 주석을 지우고 사용하세요
+	// @AllArgsConstructor가 자동 생성해주는 코드
+//	StateId(String kor){
+//		this.kor = kor;
+//	}
+	// @Getter가 자동으로 만들어주는 코드
+//	public String getKor() {
+//		return this.kor;
+//	}
 }
