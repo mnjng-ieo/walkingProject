82ae00ecd63df9bd48320e4a4ff10b59eaa0550e
yeonseo97 <dustj0824@naver.com>
2023-08-31 21:03:05 +0900

    [update] tag, boardTag 테이블 TagService로 통합

--- src/main/java/com/walk/aroundyou/service/TagService.java
+++ src/main/java/com/walk/aroundyou/service/TagService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.walk.aroundyou.domain.BoardTag;
 import com.walk.aroundyou.dto.AddTagRequest;
+import com.walk.aroundyou.repository.BoardTagRepository;
 import com.walk.aroundyou.repository.TagRepository;
 
 @Service
@@ -27,6 +29,22 @@ public class TagService {
 	// 3. 게시물 하나의 해시태그 리스트 조회하기
 	public List<String> findTagsByBoardId(Long boardId) {
 		return tagRepository.findTagsByBoardId(boardId);
+	}
+	
+	/*---------------------------------------------------*/
+	
+	@Autowired
+	private BoardTagRepository boardTagRepository;
+	
+	/// BoardTagRepository 사용하여 출력 확인하기
+	// 1. 게시물 삭제 시 board_tag 테이블에서 삭제하기
+	public void deleteByBoardTagId(Long boardTag) { 
+		boardTagRepository.deleteByBoardTagId(boardTag);
+	}
+	
+	// 2. 새로운 태그 board_tag 테이블에 추가하기
+	public void saveBoardTag(BoardTag boardTag) {
+		boardTagRepository.saveBoardTag(boardTag);
 	}
 	
 }
