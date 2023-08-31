e860bb56d249b8594fe6162d9807555509d587d4
joo0/1 <itfrombitt@gmail.com>
2023-08-31 10:52:31 +0900

    [Update] - findByBoardId()메소드 추가 - 주석 수정, 추가

---
+++ src/main/java/com/walk/aroundyou/repository/BoardCourseRepository.java
@@ -0,0 +1,36 @@
+package com.walk.aroundyou.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.walk.aroundyou.domain.Board;
+import com.walk.aroundyou.domain.BoardCourse;
+import com.walk.aroundyou.domain.Course;
+
+@Repository
+public interface BoardCourseRepository extends JpaRepository<BoardCourse, Long> {
+
+	// Board의 식별번호와 Course의 식별번호를 연결해 특정 "보드글(게시글)" 조회
+	/* 
+		하나의 산책로(courseId)에 대한 
+		게시글(boardId)은 여러 개가 나올 수 있으므로 List타입으로 지정해주었다. 
+	*/
+	@Query(value = "SELECT b.* FROM board b WHERE IN (SELECT bc.boardId FROM BoardCousre bc WHERE bc.courseId =: bc.courseId)")
+	List<Board> findByCourseId(@Param(value = "courseId") Course cousreId);
+	
+	
+	
+	
+	// Board의 식별번호와 Course의 식별번호를 연결해 특정 "산책로" 조회
+	/*
+		 하나의 게시글(boardId)에 대한 
+		 산책로(courseId)는 여러 개가 나올 수 있으므로(하나의 글에 여러 개의 산책로에 대해 쓸 수 있음) List타입으로 지정해주었다.
+	 */
+	@Query(value = "SELECT c.* FROM course c WHERE IN (SELECT bc.courseId FROM BoardCousre bc WHERE bc.boardId =: bc.boardId)")
+	List<Course> findByBoardId(@Param(value = "boardId") Board boardId);
+	
+}
