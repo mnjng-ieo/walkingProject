a7340c0e2bf8c5fa1305c6fb6e1049a500f70cd9
joo0/1 <itfrombitt@gmail.com>
2023-08-31 10:44:46 +0900

    [Update] - findByBoardId()메소드 추가 - 주석 수정, 추가

---
+++ src/main/java/com/walk/aroundyou/domain/BoardCourse.java
@@ -0,0 +1,38 @@
+6package com.walk.aroundyou.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="board_course")
+public class BoardCourse {
+
+	@Id
+	@Column(name="board_course_id", columnDefinition="bigint", nullable=false)
+	private long boardCourseId;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "board_id", nullable = false)
+	private Board boardId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "course_id", nullable = false)
+	private Course courseId;
+	
+}
