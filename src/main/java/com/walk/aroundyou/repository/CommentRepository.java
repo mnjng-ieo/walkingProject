e722941a43193811f7249083ea45ef9a4db16b03
hee9394 <dahee_lee_@naver.com>
2023-09-01 17:59:46 +0900

    Comment 관련 레포지토리, 서비스, 컨트롤러 구현

---
+++ src/main/java/com/walk/aroundyou/repository/CommentRepository.java
@@ -0,0 +1,116 @@
+package com.walk.aroundyou.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.walk.aroundyou.DTO.AddCommentRequest;
+import com.walk.aroundyou.DTO.CommentResponseDto;
+import com.walk.aroundyou.DTO.UpdateCommentRequest;
+import com.walk.aroundyou.domain.Comment;
+import com.walk.aroundyou.domain.Course;
+import com.walk.aroundyou.domainenum.CommentType;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long>{
+/* 관리자 권한에 해당하는 메서드 없음 */
+	
+	
+/* comment 조회 메서드  */
+	// 1. Board에서 board_id를 이용한 Comment 목록 조회 ( 닉네임 / 내용 / 수정날짜 / 좋아요 수 )
+	@Query(value="SELECT"
+			+ " c.user_nickname as userNickname"
+			+ ", c.comment_content as commentContent"
+			+ ", c.comment_updated_date as commentUpdatedDate"
+			+ ", IFNULL(COUNT(cl.comment_like_id), 0) AS commentLikeCnt"
+			+ "	FROM comment c"
+			+ "	LEFT JOIN comment_like cl ON c.comment_id  = cl.comment_id "
+			+ "	WHERE c.board_id = :#{#boardId}"
+			+ " GROUP BY c.comment_id ", nativeQuery = true)
+	List<CommentResponseDto> findByBoardId(@Param("boardId") Long boardId);
+	
+	
+	// 2. Course에서 course_id를 이용한 Comment 목록 조회 ( 닉네임 / 내용 / 수정날짜 / 좋아요 수 )
+	@Query(value="SELECT"
+			+ " c.user_nickname as userNickname"
+			+ ", c.comment_content as commentContent"
+			+ ", c.comment_updated_date as commentUpdatedDate"
+			+ ", IFNULL(COUNT(cl.comment_like_id), 0) AS commentLikeCnt"
+			+ "	FROM comment c"
+			+ "	LEFT JOIN comment_like cl ON c.comment_id  = cl.comment_id "
+			+ "	WHERE c.course_id = :#{#courseId}"
+			+ " GROUP BY c.comment_id ", nativeQuery = true)
+	List<CommentResponseDto> findByCourseId(@Param("courseId") Long courseId);	
+	
+	
+	
+/* comment 추가 메서드 */
+	// 1. Board에서 Comment 추가(작성) API 구현
+	@Modifying
+	@Transactional
+	@Query(value = "INSERT INTO comment "
+			+ "	(user_id, user_nickname, comment_content, board_id, comment_type)"
+			//+ "	(user_nickname, comment_content, comment_type)"
+			+ "	VALUES "
+			+ "	(:#{#create.userId.userId}, :#{#create.userNickname}, :#{#create.commentContent}, :#{#create.boardId.boardId} , 'BOARD')"
+			//+ "	(:#{#create.userNickname}, :#{#create.commentContent}, 'BOARD')"
+			, nativeQuery = true)
+	void saveCommentAsBoard(@Param("create") AddCommentRequest commentReqDto);
+	
+	
+	// 2. Course에서 Comment 추가(작성) API 구현
+	@Modifying
+	@Transactional
+	@Query(value = "INSERT INTO comment "
+			+ "	(user_id, user_nickname, comment_content, board_id, comment_type)"
+			+ "	VALUES "
+			+ "	(:#{#create.userId.userId}, :#{#create.userNickname}, :#{#create.commentContent}, :#{#create.courseId.courseId}, 'COURSE')"
+			, nativeQuery = true)
+	void saveCommentAsCourse(@Param("create") AddCommentRequest create);
+
+	
+	
+/* comment 내용 삭제 메서드 */
+	// 외래키 제한으로 인해 comment_like 먼저 삭제 후, comment 삭제해야 함 
+	
+	// 1) comment_id로 comment_like 엔티티 먼저 삭제 
+	@Transactional
+	@Modifying
+	@Query(value="DELETE FROM comment_like WHERE comment_id = :#{#commentId}", nativeQuery=true)
+	void deleteCommentByCommentLikeId(@Param("commentId") Long commentId);
+	
+	
+	// 2) comment_id로 comment 엔티티 삭제 
+	@Transactional
+	@Modifying
+	@Query(value="DELETE FROM comment WHERE comment_id = :#{#commentId}", nativeQuery = true)
+	void deleteCommentByCommentId(@Param("commentId") Long commentId);
+	
+
+	
+/* comment 내용 수정 메서드 */
+	@Query(value="UPDATE "
+			+ "comment c "
+			+ "	SET c.comment_content =:#{#update.commentContent}"
+			+ "	WHERE c.comment_id = :#{#commentId}", nativeQuery = true)
+	void updateCommentContent(@Param("commentId") Long commentId, @Param("update") UpdateCommentRequest update);
+	
+	
+	
+/* 해당 산책로(commentType = COURSE)에 관한 comment 개수 조회 메서드 */	
+	@Query(value="select "
+			+ "COUNT(c.comment_content) "
+			+ "from comment c "
+			+ "where comment_type = 'COURSE' AND c.course_id = :#{#course.courseId}"
+			, nativeQuery = true)
+	long countCourseCommentByCourseId(@Param("course") Course courseId);
+
+
+	
+}
