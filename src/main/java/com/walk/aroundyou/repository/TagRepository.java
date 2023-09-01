f30f8da27dbe050b72b0e0105edab94cb2a80d0f
yeonseo97 <dustj0824@naver.com>
2023-09-01 15:23:10 +0900

    [update] tag save() 메서드 수정 - 존재하는 tag_id는 저장되지 않게 구현 / tag관련 dto 삭제

--- src/main/java/com/walk/aroundyou/repository/TagRepository.java
+++ src/main/java/com/walk/aroundyou/repository/TagRepository.java
@@ -1,6 +1,7 @@
 package com.walk.aroundyou.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -19,23 +20,24 @@ public interface TagRepository extends JpaRepository<Tag, Long>{
 	// (기본 메서드도 사용 가능)
 	@Modifying
 	@Transactional
-	// 작동하는 쿼리문이나 기본 메서드를 사용하여 주석처리
+	void deleteByTagId(Long TagId); // 기본 메소드
+	// 작동하는 삭제 쿼리문이나 기본 메서드를 사용하여 주석처리
+	// 직접 쿼리문 보내기
 //	@Query(value = 
-//		"DELETE FROM tag WHERE tag_content = ?1", 
+//		"DELETE FROM tag WHERE tag_id = ?1", 
 //		nativeQuery = true)
-//	void deleteByTag(String tagContent); // 직접 쿼리문 보내기(매개변수가 문자열이라 사용 불가)
- 	void deleteByTagId(Long TagId); // 기본 메소드
+//	void deleteByTagId(Long tagId);
 	
 	// 2. 새로운 태그 tag 테이블에 추가하기(중복 값도 저장되니 새로운 태그인지 확인 후 사용)
 	@Modifying
 	@Transactional
 	// 외래키 객체를 사용하는 방법(DTO 객체를 만들어서 사용하기)
 	@Query(value = 
-		"INSERT INTO tag(tag_content) VALUES(?1)", 
+		"INSERT INTO tag(tag_content) VALUES( :#{#tagContent})", 
 		nativeQuery = true)
-	void saveTag(String tagContent);
+	void saveTag(@Param("tagContent") String tagContent);
 	
-	// 3. 게시물 하나에 작성된 해시태그를 조회(컨트롤러 까지 확인)
+	// 3. 게시물 하나에 작성된 해시태그를 조회
 	@Query(value = "SELECT t.tag_content "
 		+ "	   FROM tag t"
 		+ "    INNER JOIN ("
@@ -48,5 +50,15 @@ public interface TagRepository extends JpaRepository<Tag, Long>{
 		+ "		ON bb.tag_id = t.tag_id"
 		, nativeQuery = true)
 	List<String> findTagsByBoardId(@Param("boardId") Long boardId);
+	
+	// 4. 태그를 테이블에 저장할 때 존재하는 태그인지 조회하고 저장하기
+//	@Query(value = "SELECT t.*"
+//			+ "    FROM tag t "
+//			+ "    WHERE t.tag_content = :#{#tagContent}", 
+//			nativeQuery = true)
+//	Optional<Tag> findByTagContent(@Param("tagContent") String tagContent);
+	
+	// 4. 해시태그가 있는지 확인하는 메서드(기본메소드 활용, 상단의 주석 처리한 메소드와 같은 역할)
+	boolean existsByTagContent(String tagContent);
 
 }
--- src/main/java/com/walk/aroundyou/service/TagService.java
+++ src/main/java/com/walk/aroundyou/service/TagService.java
@@ -1,11 +1,13 @@
 package com.walk.aroundyou.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.walk.aroundyou.domain.BoardTag;
+import com.walk.aroundyou.domain.Tag;
 import com.walk.aroundyou.dto.AddTagRequest;
 import com.walk.aroundyou.repository.BoardTagRepository;
 import com.walk.aroundyou.repository.TagRepository;
@@ -25,13 +27,33 @@ public class TagService {
 	}
 	
 	// 2. 새로운 태그 tag 테이블에 추가하기
-	public void saveTag(AddTagRequest request) {
-		 tagRepository.saveTag(request.getTagContent());
+	// saveTag메소드 DTO 사용에서 엔티티 클래스 활용하는 방식으로 변경
+	/// 변경 전
+//	public void saveTag(AddTagRequest request) { 
+//		tagRepository.saveTag(request.getTagContent());
+//	}
+	/// 변경 후
+	// 동일한 태그가 있으면 tag 테이블에 저장하지 않는 방식
+	public void saveTag(String tagContent) { 
+		Tag hashTag = new Tag();
+		hashTag.setTagContent(tagContent);
+		
+		if(tagRepository.existsByTagContent(tagContent)) {
+			// 해당 tagContent가 존재하는 경우 저장하지 않음			
+		} else {
+			// 해당 tagContent가 존재하지 않는 경우 저장
+			tagRepository.saveTag(tagContent);
+		}
 	}
 	
 	// 3. 게시물 하나의 해시태그 리스트 조회하기
 	public List<String> findTagsByBoardId(Long boardId) {
 		return tagRepository.findTagsByBoardId(boardId);
+	}
+	
+	// 4. 태그를 테이블에 저장할 때 존재하는 태그인지 조회하고 저장하기
+	public void saveBoardTagAndTag(String TagContents) {
+		// for(Optional<Tag> optionalTag : TagContents ) {}
 	}
 	
 	/*---------------------------------------------------*/
