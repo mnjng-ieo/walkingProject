549fe7d6f232eee21278b808be4c454e67dfca0a
Juyoung Lym <itfrombitt@gmail.com>
2023-09-05 21:11:57 +0900

    수정

--- src/main/java/com/walk/aroundyou/controller/BoardLikeApiController.java
+++ src/main/java/com/walk/aroundyou/controller/BoardLikeApiController.java
@@ -1,7 +1,6 @@
 package com.walk.aroundyou.controller;
 
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
