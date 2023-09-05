b5cfa800551ee709a19f74d3dcafb53685988bfe
yeonseo97 <dustj0824@naver.com>
2023-09-05 11:53:07 +0900

    [update] board css와 충돌하는 이름 파일명 재수정

--- src/main/java/com/walk/aroundyou/controller/ViewController.java
+++ src/main/java/com/walk/aroundyou/controller/ViewController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.walk.aroundyou.domain.Board;
 import com.walk.aroundyou.dto.BoardRequest;
@@ -46,20 +47,21 @@ public class ViewController {
 		List<String> hotTagList = 
 			tagService.findTagsByBoardTagId();
 		model.addAttribute("hotTagList", hotTagList);
-		return "hotTagList";
+		return "hotTag";
 	}
 	
 	// 하나의 게시물에 해당하는 해시태그 리스트 출력하기
 	@GetMapping("/tagList/{boardId}")
 	// @PathVariable 어노테이션을 사용하여 URL에서 추출한 boardId를 파라미터로 전달
 	public String tagListInBoardContent(@PathVariable Long boardId, Model model) {
+		log.info("/tagList/{boardId} 접근 .... ");
 		// 존재하지 않는 boardId를 조회할때도 대비하기, 아직 구현하지 않음
 		Optional<Board> boardContent = boardRepository.findById(boardId);
 		List<String> boardTagList = 
 			tagService.findTagsByBoardId(boardId);
 		model.addAttribute("boardContent", boardContent.get().getBoardContent());
 		model.addAttribute("boardTagList", boardTagList);
-		return "boardtag";
+		return "boardTag";
 	}
 	
 	// 게시물 저장하면 태그도 저장하기(작성 중)
@@ -73,15 +75,16 @@ public class ViewController {
         return ResponseEntity.ok().build();
     } 
 	
-	@GetMapping("/boardTagList/{tagContent}")
+	// 하나의 해시태그가 가진 게시물 리턴
+	@GetMapping("/tagBoardList/{tagContent}")
 	// @PathVariable 어노테이션을 사용하여 URL에서 추출한 boardId를 파라미터로 전달
 	public String boardListIntagContent(@PathVariable("tagContent") String tagContent, Model model) {
 		log.info("tagContent = {}", tagContent);
 		// 존재하지 않는 tagContent를 조회할때도 대비하기, 아직 구현하지 않음
-		List<Board> boardTagList = 
+		List<Board> tagBoardList = 
 			tagService.findBoardByTag(tagContent);
-		model.addAttribute("boardTagList", boardTagList);
-		return "boardTagList";
+		model.addAttribute("tagBoardList", tagBoardList);
+		return "tagBoardList";
 	}
 	
 }
