239f2a65faf52ea3d864500d4ba69635dafddcea
hanaBBun <iamjeesoo@gmail.com>
2023-09-06 15:25:49 +0900

    [update]courseList.js!

--- src/main/resources/static/lib/courseList.js
+++ src/main/resources/static/lib/courseList.js
@@ -52,3 +52,9 @@ function loadPage(newPage) {
 function loadReset() {
     window.location.href = '/course';
 }
+
+// 각 상세페이지로 경로 넘어가는 클릭 이벤트
+function redirectToCourseDetails(courseId){
+    let url = "/course/" + courseId;
+    window.location.href = url;
+}
