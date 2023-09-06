2ef223ab44192e1088b66d584a8c59a313f8c946
hanaBBun <iamjeesoo@gmail.com>
2023-09-06 21:52:14 +0900

    [0906]관리자페이지 산책로데이터관리 구현중

--- src/main/java/com/walk/aroundyou/controller/CourseViewController.java
+++ src/main/java/com/walk/aroundyou/controller/CourseViewController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,11 +32,8 @@ public class CourseViewController {
 	/**
 	 * [산책로 목록 조회페이지] 전체 목록 조회
 	 *  ↳ REST로는 다수의 파라미터를 넘겨서 검색 조건이 되도록 했다.
-	 *    뷰에서는 경로에 나타나지 않고 AJAX나 POST 요청으로 처리하면 좋을 것 같은데,
-	 *    파라미터였던 검색 조건들을 하나의 dto 객체로 만들어서
-	 *    Requestbody에 실는 것이 가능한가? ... 
-	 *    시도에 오랫동안 실패하고 처음 전체 조회 화면과 검색 결과를 분리하여
-	 *    GET 방식 요청 처리로 만들었다.
+	 *    뷰에서는 경로에 나타나지 않고 AJAX로 처리!
+	 *    이 메소드는 ajax와 관계없이 전체 조회 첫 페이지 요청
 	 */
 	@GetMapping("/course")
 	public String getCourses(
@@ -82,8 +80,7 @@ public class CourseViewController {
 
 	/**
 	 * [산책로 목록 조회페이지] 검색 조건에 따른 전체 목록 조회
-	 * : 메뉴를 클릭했을 때 나오는 첫 화면과 별개로 POST 요청을 처리해야 할 것 같아서 만들었다.
-	 *   아직 완성 처리되지 않음.
+	 * : 메뉴를 클릭했을 때 나오는 첫 화면과 별개로 조건에 따른 AJAX 요청 경로 처리
 	 */
 	@GetMapping("/course/search")
 	public String getCoursesByConditions(
@@ -199,16 +196,113 @@ public class CourseViewController {
 		return result;
 	}
 	
-//	/**
-//	 * pagination의 마지막 숫자 얻는 메소드
-//	 */
-//	private int getPageEnd(int pageStart, int totalPages) {
-//		int result = pageStart + PAGINATION_SIZE - 1;
-//		if(totalPages < result) {
-//			result = totalPages;
-//			
-//		}
-//		return result;
-//	}
-	
+	/**
+	 * [관리자 페이지] 산책로 데이터 관리 - 산책로 목록 조회
+	 * : 관리자만 볼 수 있는 탭의 산책로 데이터 관리 메뉴에서는
+	 *   사용자와 달리 더 심플하게 구성되어 있다.
+	 */
+	@GetMapping("/admin/courses")
+	public String adminGetCourses(
+			@RequestParam(name = "region", required = false) String region, 
+			@RequestParam(name = "level", required = false) String level, 
+			@RequestParam(name = "time", required = false) String time,
+			@RequestParam(name = "distance", required = false) String distance, 
+			@RequestParam(name = "searchTargetAttr", required = false) String searchTargetAttr,
+			@RequestParam(name = "searchKeyword", required = false) String searchKeyword, 
+			@RequestParam(name="sort", required= false) String sort,
+			@RequestParam(name="page", required= false, 
+			defaultValue = "0") int currentPage,
+			Model model) {
+			
+			String startTime = null;
+			String endTime = null; 
+			
+			if(time == null) {
+				
+			} else if(time.equals("1")) {
+				startTime = "00:00:00";
+				endTime = "00:59:00";
+		    } else if (time.equals("2")) {
+		    	startTime = "01:00:00";
+		    	endTime = "01:59:00";
+		    } else if (time.equals("3")) {
+		    	startTime = "02:00:00";
+		    	endTime = "02:59:00";
+		    } else if (time.equals("4")) {
+		    	startTime = "03:00:00";
+		    	endTime = "03:59:00";
+		    } else if (time.equals("5")) {
+		    	startTime = "04:00:00";
+		    	endTime = "99:00:00";
+		    }
+			
+		Page<Course> coursePage = 
+				courseService.findAllByCondition(
+				region, level, distance, startTime, endTime,
+				searchTargetAttr, searchKeyword, sort, currentPage);
+		
+		List<CourseResponseDTO> courses = coursePage
+				.stream()
+				.map(course -> {
+					CourseResponseDTO dto = new CourseResponseDTO(course);
+					// 좋아요 수, 언급 수, 댓글 수 추가
+					dto.setLikeCnt(
+							courseRepository.countCourseLikesByCourseId(
+									course.getCourseId()));;
+					dto.setMentionCnt(
+							courseRepository.countCourseMentionsByCourseId(
+									course.getCourseId()));
+					dto.setCommentCnt(
+							courseRepository.countCourseCommentsByCourseId(
+									course.getCourseId()));;
+					return dto;
+				})
+				.toList();
+		
+		// pagination 설정
+		// 헷갈리지 말자 : currentPage는 0부터 시작!
+		int totalPages = coursePage.getTotalPages();
+		int pageStart = getPageStart(currentPage, totalPages);
+		int pageEnd = 
+				(PAGINATION_SIZE < totalPages)? 
+						pageStart + PAGINATION_SIZE - 1
+						:totalPages;
+		model.addAttribute("lastPage", totalPages);
+		// 이전, 이후 버튼에 문제있어서 currentPage + 1 값을 수정했다.
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("pageStart", pageStart);
+		model.addAttribute("pageEnd", pageEnd);
+		
+		// 산책로 리스트 저장
+		model.addAttribute("courses", courses);  
+		
+		// 파라미터 값을 모델에 저장 (페이지네이션에 쓰임) - 알아요 쓸데없이 긴 거ㅠㅠ
+		model.addAttribute("region", region);
+		model.addAttribute("level", level);
+		model.addAttribute("distance", distance);
+		model.addAttribute("startTime", startTime);
+		model.addAttribute("endTime", endTime);
+		model.addAttribute("searchTargetAttr", searchTargetAttr);
+		model.addAttribute("searchKeyword", searchKeyword);
+		model.addAttribute("sort", sort);
+		
+		// courseList.html라는 뷰 조회
+		return "adminCourseList";
+	}
+
+	/**
+	 * [관리자 페이지] 산책로 데이터 관리 - 상세 조회 페이지
+	 */
+	@GetMapping("/admin/courses/{id}")
+	public String adminGetCourse(@PathVariable Long id, Model model) {
+		CourseResponseDTO courseResponseDTO = courseService.findByIdWithCounts(id);
+		model.addAttribute("course", courseResponseDTO);
+		
+		return "adminCourse";
+	}
+
+
+
 }
+
+	
--- src/main/java/com/walk/aroundyou/service/CourseService.java
+++ src/main/java/com/walk/aroundyou/service/CourseService.java
@@ -28,7 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 public class CourseService {
 
 	// 페이징 처리에 필요한, 한 페이지에 조회되는 데이터 수
-	private final static int SIZE_OF_PAGE = 20;
+	private final static int SIZE_OF_PAGE = 12;
 	private final CourseRepository courseRepository;
 	
 	/**
