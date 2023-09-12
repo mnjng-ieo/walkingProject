a8e848874d5f5839c82fd4abdd54450f4e3f4d67
yeonseo97 <dustj0824@naver.com>
2023-09-12 20:37:11 +0900

    [update] 민정언니 파일과 합본

--- src/main/java/com/walk/aroundyou/controller/BoardViewController.java
+++ src/main/java/com/walk/aroundyou/controller/BoardViewController.java
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.walk.aroundyou.domain.Board;
 import com.walk.aroundyou.domain.Comment;
 import com.walk.aroundyou.domain.Course;
 import com.walk.aroundyou.domain.Member;
+import com.walk.aroundyou.domain.Tag;
 import com.walk.aroundyou.domainenum.BoardType;
 import com.walk.aroundyou.dto.BoardRequest;
 import com.walk.aroundyou.dto.IBoardDetailResponse;
@@ -176,7 +178,6 @@ public class BoardViewController {
 		return "boardDetail";
 	}
 	
-	
 	// 게시물 삭제
 	@PostMapping("/board/{id}")
 	public String getBoardDelete(@PathVariable Long id, Model model) {
@@ -186,63 +187,57 @@ public class BoardViewController {
 	
 	// 게시물 작성 폼
 	@GetMapping("/board-editor")
-	public String getBoardForm() {
+	public String getBoardForm(Model model, Optional<Long> course) {
+		// 산책로 지역 선택 항목 가져오기
+		List<String> allSignguCn = courseService.findAllSignguCn();
+		model.addAttribute("allSignguCn", allSignguCn);
+		if(course.isPresent()) {
+			Course resultCourse = courseService.findById(course.get());
+			model.addAttribute("courseId", resultCourse.getCourseId());
+			model.addAttribute("wlkCoursFlagNm", resultCourse.getWlkCoursFlagNm());
+			model.addAttribute("wlkCoursNm", resultCourse.getWlkCoursNm());
+		}
+		
 		return "boardForm";
 	}
-	
-	// 게시물 작성
-	@PostMapping("/board-editor")
-	public String postBoardForm(String boardType, String boardTitle, String boardContent) {
-		// 뷰에서 유저 데이터 불러오는 로직 대체
-		BoardRequest form = BoardRequest.builder()
-				.boardType(BoardType.valueOf(boardType))
-				.boardTitle(boardTitle)
-				.boardContent(boardContent)
-				.userId("wayid10")
-				.userNickname("가나다")
-				.build();
-
-		log.info(form.toString());
-		if(boardService.save(form)) {
-			log.info("게시물 저장 성공");
-			return "redirect:/board";			
-		} else {
-			log.info("게시물 저장 실패");
-			return "redirect:/board-editor";
-		}	
-	}
-	
 	
 	// 게시물 수정 폼
 	@GetMapping("/board-editor/{id}")
 	public String getBoardFormById(@PathVariable Long id, Model model) {
 		Optional<IBoardDetailResponse> board = boardService.findBoardDetail(id);
 		model.addAttribute("board", board.get());
+		List<String> allSignguCn = courseService.findAllSignguCn();
+		model.addAttribute("allSignguCn", allSignguCn);
+		Optional<Course> course = courseService.findByBoardId(id);
+		if(course.isPresent()) {
+			log.info("course 값이 있음");
+			Course resultCourse = course.get();
+			model.addAttribute("courseId", resultCourse.getCourseId());
+			model.addAttribute("wlkCoursFlagNm", resultCourse.getWlkCoursFlagNm());
+			model.addAttribute("wlkCoursNm", resultCourse.getWlkCoursNm());
+		} else {
+			log.info("course 값이 없음");	
+		}
+		if(course.isPresent()) {
+		}
 		return "boardFormById";
 	}
 	
-	// 게시물 수정
-	@PostMapping("/board-editor/{id}")
-	public String postBoardFormById(String boardType, String boardTitle, String boardContent,@PathVariable Long id) {
-		// 뷰에서 유저 데이터 불러오는 로직 대체
-		BoardRequest form = BoardRequest.builder()
-				.boardId(id)
-				.boardType(BoardType.valueOf(boardType))
-				.boardTitle(boardTitle)
-				.boardContent(boardContent)
-				.userId("wayid10")
-				.userNickname("가나다")
-				.build();
-
-		log.info(form.toString());
-		if(boardService.save(form)) {
-			log.info("게시물 저장 성공");
-			return "redirect:/board/"+id;			
-		} else {
-			log.info("게시물 저장 실패");
-			return "redirect:/board-editor/"+id;
+	
+	// 하나의 게시물에 포함된 해시태그 리스트 출력하기
+		@GetMapping("/tagList/{boardId}")
+		// @PathVariable 어노테이션을 사용하여 URL에서 추출한 boardId를 파라미터로 전달
+		public String tagListInBoardContent(@PathVariable Long boardId, Model model) {
+			log.info("/tagList/{boardId} 접근 .... ");
+			// 존재하지 않는 boardId를 조회할때도 대비하기, 아직 구현하지 않음
+			Optional<Board> boardContent = boardService.findById(boardId);
+			List<String> boardTagList = 
+				tagService.findTagsByBoardId(boardId);
+			model.addAttribute("boardContent", boardContent.get().getBoardContent());
+			model.addAttribute("boardTagList", boardTagList);
+			return "boardTag";
 		}	
-	}
+	
 	
 	/////////////// 댓글
 	// 댓글 
@@ -278,16 +273,18 @@ public class BoardViewController {
 	
 	
 	
-	// pagination의 시작 숫자 얻는 메소드
+	// 페이지네이션 시작 페이지를 계산해주는 컨트롤러
 	private int getPageStart(int currentPage, int totalPages) {
-		
+		log.info("currentPage = {}, totalPages = {}", currentPage, totalPages);
 		int result = 1;
 		if(totalPages < currentPage + (int)Math.ceil((double)PAGINATION_SIZE/2)) {
-			// 시작페이지의 최소값은 1!
-			result = Math.max(1, totalPages - PAGINATION_SIZE + 1);
-		} else if (currentPage > (int)Math.floor((double)PAGINATION_SIZE/2)) {
+			log.info("if문 통과");
+			result = totalPages - PAGINATION_SIZE + 1;
+		}else if(currentPage > (int)Math.floor((double)PAGINATION_SIZE/2)) {
 			result = currentPage - (int)Math.floor((double)PAGINATION_SIZE/2) + 1;
+			log.info("else if문 통과");
 		}
+		
 		return result;
 	}
 }
