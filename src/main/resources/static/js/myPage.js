cfbb0d611c802054226e99f21191c5833d715961
hanaBBun <iamjeesoo@gmail.com>
2023-09-14 16:07:20 +0900

    [0914] mypage

--- src/main/resources/static/js/myPage.js
+++ src/main/resources/static/js/myPage.js
@@ -7,16 +7,28 @@ window.onload = function() {
     // 내소개 수정 버튼을 눌렀을 때 - 수정폼 태그만 남는다.
     $("#modifyBtn").on("click",
     function(){
-        //$(this).parent().parent().parent().parent().parent().parent().css('display', 'none');
-        //$(this).parent().parent().parent().parent().parent().parent().next().css('display', 'block');
+        $("#getUserBox").css('display', 'none');
+        $("#modifyUserBox").css('display', 'block');
     })
     
     // 수정완료 버튼을 눌렀을 때 - 조회 태그만 남는다.
     $("#modifySuccessBtn").on("click",
     function(){
-        //$(this).parent().parent().parent().parent().parent().parent().css('display', 'none');
-        //$(this).parent().parent().parent().parent().parent().parent().prev().css('display', 'block');
+        $("#modifyUserBox").css('display', 'none');
+        $("#getUserBox").css('display', 'block');
     })
+    
+    // 이미지가 '/images/defaultUserImage.png'인 경우 uploadBtn이, 아니면 deleteBtn 뜨도록 
+    const userProfileImage = document.getElementById('user-imgBox_image');
+    const imageUploadBtn = document.querySelector('.imageUploadButton');
+    const imageDeleteBtn = document.querySelector('.imageDeleteButton');
+    if(userProfileImage.src.endsWith('/images/defaultUserImage.png')){
+        imageUploadBtn.style.display = 'block';
+        imageDeleteBtn.style.display = 'none';
+    } else {
+        imageUploadBtn.style.display = 'none';
+        imageDeleteBtn.style.display = 'block';
+    }
     
 }
 
@@ -45,6 +57,13 @@ function uploadImage() {
                 userProfileImage.src = e.target.result;  // 읽은 파일의 데이터
             };
             reader.readAsDataURL(userFile);
+            
+            // uploadBtn은 사라지고 deleteBtn이 뜨도록 
+            const imageUploadBtn = document.querySelector('.imageUploadButton');
+            const imageDeleteBtn = document.querySelector('.imageDeleteButton');
+            imageUploadBtn.style.display = 'none';
+            imageDeleteBtn.style.display = 'block';
+            
         } else {
             // 파일 선택이 취소되었을 경우 file을 어떻게 제거할까? - deleteImage()
         }
@@ -65,4 +84,77 @@ function deleteImage() {
     
     // file 변수를 초기화(삭제)
     userFile = null;
+    
+    // uploadBtn은 나타나고 deleteBtn이 사라지도록
+    const imageUploadBtn = document.querySelector('.imageUploadButton');
+    const imageDeleteBtn = document.querySelector('.imageDeleteButton');
+    imageUploadBtn.style.display = 'block';
+    imageDeleteBtn.style.display = 'none';
+}
+
+/////////////////////////////////////////////////////////////
+$(document).ready(function () {
+    // 첫 페이지에서 게시물만 보이게
+    $('#imgContainer').collapse('hide');
+    $('#postContainer').collapse('show');
+    
+    // imgBtn 버튼을 클릭할 때
+    $('#imgBtn').on('click', function () {
+        // 열려있는지 확인
+        if (!$('#imgContainer').hasClass('show')) {
+            $('#postContainer').collapse('hide');
+            $('#imgContainer').collapse('show');
+        }
+        
+    });
+    
+    // postBtn 버튼을 클릭할 때
+    $('#postBtn').on('click', function () {
+        // id=imgContainer가 열려 있다면 닫기
+
+        // id=postContainer가 닫혀 있다면 열기
+        if (!$('#postContainer').hasClass('show')) {
+            $('#postContainer').collapse('show');
+            $('#imgContainer').collapse('hide');
+        }
+    });
+});
+
+    
+// 내가 쓴 게시글 클릭하여 게시물 상세보기
+function redirectToDetail(row) {
+    let url = row.getAttribute("data-url");
+            
+    // 페이지 이동
+    window.location.href = url;
+}
+
+// 페이지를 로드할 때 초기 데이터를 불러온다.
+function loadPage(pageNumber) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) { // 요청이 완료됨
+            if (xhr.status === 200) { // 요청이 성공적으로 처리됨
+            
+            let response = xhr.responseText;
+            // response를 파싱해서 searchResults div 내용만 추출
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(response, 'text/html');
+            let searchResultsContent = 
+                    doc.querySelector('#needToChange').innerHTML;
+                    
+            // searchResults div 내용 교체
+            document.getElementById("needToChange").innerHTML = searchResultsContent;                      
+
+            } else {
+                // 요청이 실패한 경우의 처리
+                console.log('Error:', xhr.status, xhr.statusText);
+            }
+        }
+    };
+
+    // GET 요청을 보냄
+    xhr.open('GET', '/main/mypage?page=' + pageNumber, true);
+    xhr.send();
 }
