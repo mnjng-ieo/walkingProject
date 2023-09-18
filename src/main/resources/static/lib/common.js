61cc3d39a50f1985ef2917d3a01284afc84dc35b
yeonseo97 <dustj0824@naver.com>
2023-09-18 16:12:37 +0900

    [update] 헤더 변경본

--- src/main/resources/static/lib/common.js
+++ src/main/resources/static/lib/common.js
@@ -4,16 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
    
    searchBtn.addEventListener("click", function () {
       //alert('돋보기 클릭');
-      if (searchBox.style.height == "100px") {
+      if (searchBox.style.height == "70px") {
            // 콜랩스를 숨김
            //alert('searchBox 숨김')
            searchBox.style.height = "0px";
       } else {
          // 콜랩스를 보이게 함
            //alert('searchBox 보이게 함');
-           searchBox.style.height = "100px";
-           searchBox.style.position = "absolute";
-           searchBOx.style.top = "-100%";
+           searchBox.style.height = "70px";
       }
    });
 });
